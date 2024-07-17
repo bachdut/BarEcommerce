1e2e5de031ba112c5aefdd9fadaabc33269018fd
bachdut <barbaron117@gmail.com>
2024-07-17 03:13:37 +0300

    workingVer

---
+++ deploy_application.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Function to wait until all pods are running
+wait_for_pods() {
+  echo "Waiting for all pods to be in Running status and Ready condition..."
+  while true; do
+    not_ready_pods=$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name} {.status.phase} {.status.containerStatuses[0].ready}{"\n"}{end}' | grep -v 'Running true')
+    if [ -z "$not_ready_pods" ]; then
+      echo "All pods are running and ready."
+      echo "Application deployed successfully."
+      break
+    else
+      echo "The following pods are not yet running or not ready:"
+      echo "$not_ready_pods"
+      echo "Waiting for 10 seconds before checking again..."
+      sleep 10
+    fi
+  done
+}
+
+# Deploy MongoDB
+kubectl apply -f mongodb/mongodb-deployment.yaml
+kubectl apply -f mongodb/mongodb-service.yaml
+
+# Deploy Kafka
+kubectl apply -f kafka/kafka-deployment.yaml
+kubectl apply -f kafka/kafka-service.yaml
+
+# Deploy Zookeeper
+kubectl apply -f kafka/zookeeper-deployment.yaml
+kubectl apply -f kafka/zookeeper-service.yaml
+
+# Deploy Metrics Server
+kubectl apply -f metrics-server/metrics-server-deployment.yaml
+kubectl apply -f metrics-server/metrics-server-service.yaml
+
+# Deploy Customer UI
+kubectl apply -f customer-ui/customer-ui-deployment.yaml
+kubectl apply -f customer-ui/customer-ui-service.yaml
+
+# Deploy Customer Facing Web Server
+kubectl apply -f customer-facing-web-server/customer-facing-web-server-deployment.yaml
+kubectl apply -f customer-facing-web-server/customer-facing-web-server-service.yaml
+kubectl apply -f autoscaling/customer-facing-web-server-hpa.yaml
+
+# Deploy Customer Management API
+kubectl apply -f customer-management-api/customer-management-api-deployment.yaml
+kubectl apply -f customer-management-api/customer-management-api-service.yaml
+kubectl apply -f autoscaling/customer-management-api-hpa.yaml
+
+# Deploy API Gateway
+kubectl apply -f api-gateway/apigateway-config-map.yaml
+kubectl apply -f api-gateway/api-gateway-deployment.yaml
+kubectl apply -f api-gateway/api-gateway-service.yaml
+
+wait_for_pods
---
+++ shutdown_application.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+check_pods_deleted() {
+  echo "Checking if all pods are deleted..."
+  while true; do
+    pod_count=$(kubectl get pods --no-headers | wc -l)
+    if [ "$pod_count" -eq 0 ]; then
+      echo "All pods deleted successfully."
+      break
+    else
+      echo "Waiting for pods to be deleted..."
+      sleep 5
+    fi
+  done
+}
+
+# Delete Customer Management API
+kubectl delete -f autoscaling/customer-management-api-hpa.yaml
+kubectl delete -f customer-management-api/customer-management-api-service.yaml
+kubectl delete -f customer-management-api/customer-management-api-deployment.yaml
+
+# Delete Customer Facing Web Server
+kubectl delete -f autoscaling/customer-facing-web-server-hpa.yaml
+kubectl delete -f customer-facing-web-server/customer-facing-web-server-service.yaml
+kubectl delete -f customer-facing-web-server/customer-facing-web-server-deployment.yaml
+
+# Delete Customer UI
+kubectl delete -f customer-ui/customer-ui-service.yaml
+kubectl delete -f customer-ui/customer-ui-deployment.yaml
+
+# Delete Metrics Server
+kubectl delete -f metrics-server/metrics-server-service.yaml
+kubectl delete -f metrics-server/metrics-server-deployment.yaml
+
+# Delete Zookeeper
+kubectl delete -f kafka/zookeeper-service.yaml
+kubectl delete -f kafka/zookeeper-deployment.yaml
+
+# Delete Kafka
+kubectl delete -f kafka/kafka-service.yaml
+kubectl delete -f kafka/kafka-deployment.yaml
+
+# Delete MongoDB
+kubectl delete -f mongodb/mongodb-service.yaml
+kubectl delete -f mongodb/mongodb-deployment.yaml
+
+# Deploy API Gateway
+kubectl delete -f api-gateway/apigateway-config-map.yaml
+kubectl delete -f api-gateway/api-gateway-deployment.yaml
+kubectl delete -f api-gateway/api-gateway-service.yaml
+
+check_pods_deleted
